4fbf73d02c1b56f43d7a7f87535d524fbb553748
oeway <oeway007@gmail.com>
2018-06-23 00:10:27 +0200

    update home

--- web/src/App.vue
+++ web/src/App.vue
@@ -33,7 +33,7 @@ export default {
       // window.addEventListener("message", receiveMessage, false);
       // function receiveMessage(event)
       // {
-      //   if (event.origin !== "http://localhost:8000" || event.origin !== "https://shareloc.xyz")
+      //   if (event.origin !== "http://localhost:8000" || event.origin !== "https://imjoy.io")
       //     return;
       //   this.store.event_bus.$emit('message', event)
       // }
--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -58,7 +58,7 @@ export default {
   methods: {
     shareOnTwitter() {
       const url = "https://imjoy.io";
-      const text = " #ShareLoc ";
+      const text = " #ImJoy ";
       window.open('http://twitter.com/share?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
     }
 
