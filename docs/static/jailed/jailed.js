a9e548a013e5c8d84ffa2f9934308630ce531826
oeway <oeway007@gmail.com>
2018-07-01 15:37:51 +0200

    deploy

--- docs/static/jailed/jailed.js
+++ docs/static/jailed/jailed.js
@@ -429,13 +429,121 @@ function randId() {
 
     }
 
+    /**
+     * Creates the platform-dependent SocketioConnection object in the
+     * web-browser environment
+     */
+    var SocketioConnectionWeb = function() {
+
+
+        /**
+         * Platform-dependent implementation of the BasicConnection
+         * object, initializes the plugin site and provides the basic
+         * messaging-based connection with it
+         *
+         * For the web-browser environment, the plugin is created as a
+         * Worker in a sandbaxed frame
+         */
+        SocketioConnection = function(id, mode, context) {
+            this._init = new Whenable;
+            this._disconnected = false;
+            this.id = id;
+            this.context = context
+            if (!this._disconnected) {
+              this.context.socket.on('message_from_plugin_'+this.id,  (data)=>{
+                  console.log('message_from_plugin_'+this.id, data)
+                  if (data.type == 'initialized') {
+                      this.dedicatedThread =
+                          data.dedicatedThread;
+                      this._init.emit();
+                  } else {
+                      this._messageHandler(data);
+                  }
+              })
+            }
+            // create a plugin here
+            this.context.socket.emit('init_plugin', {id: id, mode: mode}, (result) => {
+              console.log('init_plugin: ', result)
+              if(result.success){
+                this.secret = result.secret
+              }
+              else{
+                console.error('failed to initialize plugin on the plugin engine')
+                throw('failed to initialize plugin on the plugin engine')
+              }
+            })
+
+        }
+
+
+        /**
+         * Sets-up the handler to be called upon the SocketioConnection
+         * initialization is completed.
+         *
+         * For the web-browser environment, the handler is issued when
+         * the plugin worker successfully imported and executed the
+         * _pluginWebWorker.js or _pluginWebIframe.js, and replied to
+         * the application site with the initImprotSuccess message.
+         *
+         * @param {Function} handler to be called upon connection init
+         */
+        SocketioConnection.prototype.whenInit = function(handler) {
+            this._init.whenEmitted(handler);
+        }
+
+
+        /**
+         * Sends a message to the plugin site
+         *
+         * @param {Object} data to send
+         */
+        SocketioConnection.prototype.send = function(data) {
+            this.context.socket.emit('message_to_plugin_'+this.id,
+                {type: 'message', data: data}
+            );
+            console.log('message_to_plugin_'+this.id, {type: 'message', data: data})
+        }
+
+
+        /**
+         * Adds a handler for a message received from the plugin site
+         *
+         * @param {Function} handler to call upon a message
+         */
+        SocketioConnection.prototype.onMessage = function(handler) {
+            this._messageHandler = handler;
+        }
+
+
+        /**
+         * Adds a handler for the event of plugin disconnection
+         * (not used in case of Worker)
+         *
+         * @param {Function} handler to call upon a disconnect
+         */
+        SocketioConnection.prototype.onDisconnect = function(){};
+
+
+        /**
+         * Disconnects the plugin (= kills the frame)
+         */
+        SocketioConnection.prototype.disconnect = function() {
+            if (!this._disconnected) {
+                this._disconnected = true;
+            }
+        }
+
+    }
+
 
     if (__is__node__) {
         initNode();
         basicConnectionNode();
+        SocketioConnectionWeb();
     } else {
         initWeb();
         basicConnectionWeb();
+        SocketioConnectionWeb();
     }
 
 
@@ -448,8 +556,13 @@ function randId() {
      * methods for loading scripts and executing the given code in the
      * plugin
      */
-    var Connection = function(id, mode, iframe_container, iframe_window){
-        this._platformConnection = new BasicConnection(id, mode, iframe_container, iframe_window);
+    var Connection = function(id, mode, iframe_container, iframe_window, context){
+        if(mode == 'pyworker'){
+          this._platformConnection = new SocketioConnection(id, mode, context);
+        }
+        else{
+          this._platformConnection = new BasicConnection(id, mode, iframe_container, iframe_window);
+        }
 
         this._importCallbacks = {};
         this._executeSCb = function(){};
@@ -672,7 +785,7 @@ function randId() {
             me._fail.emit();
             me.disconnect();
         }
-        this._connection = new Connection(this.id, this.mode, this.config.iframe_container, this.config.iframe_window);
+        this._connection = new Connection(this.id, this.mode, this.config.iframe_container, this.config.iframe_window, this.config.context);
         this._connection.whenInit(function(){
             me._init();
         });
