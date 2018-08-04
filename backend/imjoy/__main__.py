d76f475a5ee0936efdb8d8bb4d2e348d85fe3ea5
oeway <oeway007@gmail.com>
2018-08-04 16:42:23 +0200

    add message

--- backend/imjoy/__main__.py
+++ backend/imjoy/__main__.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     try:
         import pkg_resources  # part of setuptools
-        version = pkg_resources.require("MyProject")[0].version
+        version = pkg_resources.require("imjoy")[0].version
         print('ImJoy Python Plugin Engine (version {})'.format(version))
     except:
         pass
