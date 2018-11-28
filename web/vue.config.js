e7f9070c6bb6d01cc868e2b2d5136f8c60da6a82
oeway <oeway007@gmail.com>
2018-11-29 00:05:45 +0100

    temporary fix for shortcuts in the  monaco editor

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,8 +1,21 @@
+// vue.config.js
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const webpack = require('webpack')
 module.exports = {
   runtimeCompiler: true,
   outputDir: '../docs',
   devServer: {
     compress: true,
     port: 8000
+  },
+  configureWebpack: {
+    plugins: [
+      new MonacoWebpackPlugin(),
+      new webpack.DefinePlugin({
+        //bypass process check, https://github.com/Microsoft/monaco-editor-webpack-plugin/issues/28
+        //TODO: remove this when the bug is fixed
+        'process.platform': 0
+      })
+    ]
   }
 }
