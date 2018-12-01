9f8a7beb607730786b2a123ee0d580d563c9a007
oeway <oeway007@gmail.com>
2018-12-01 08:46:01 +0100

    fix build scripts with missing files

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,8 @@
 // vue.config.js
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const path = require('path')
 const webpack = require('webpack')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const CopyWebpackPlugin = require('copy-Webpack-plugin')
 module.exports = {
   runtimeCompiler: true,
   outputDir: './dist',
@@ -10,6 +12,16 @@ module.exports = {
   },
   configureWebpack: {
     plugins: [
+      // new CopyWebpackPlugin([{
+      //   from: path.join(__dirname, "public/docs"),
+      //   to: path.join(__dirname, "dist/docs"),
+      //   toType: "dir",
+      //   ignore: [ ".DS_Store" ]
+      // },{
+      //   from: path.join(__dirname, "public/.nojekyll"),
+      //   to: path.join(__dirname, "dist/.nojekyll"),
+      //   toType: "file"
+      // }]),
       new MonacoWebpackPlugin(),
       new webpack.DefinePlugin({
         //bypass process check, https://github.com/Microsoft/monaco-editor-webpack-plugin/issues/28
