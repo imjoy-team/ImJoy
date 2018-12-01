0597f0154b595ba5800d75199d83b0d7c2c584d4
Wei Ouyang <oeway007@gmail.com>
2018-12-01 08:23:35 +0000

    Fix missing files during build (#35)
    * fix build scripts with missing files
    
    * copy ignored index.html file
    
    * add copy-webpack-plugin
    
    * fix typo
    
    * migrate to travis.com

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,8 @@
 // vue.config.js
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const path = require('path')
 const webpack = require('webpack')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   runtimeCompiler: true,
   outputDir: './dist',
@@ -10,6 +12,11 @@ module.exports = {
   },
   configureWebpack: {
     plugins: [
+      new CopyWebpackPlugin([{
+        from: path.join(__dirname, "public/docs/index.html"),
+        to: path.join(__dirname, "dist/docs/index.html"),
+        toType: "file"
+      }]),
       new MonacoWebpackPlugin(),
       new webpack.DefinePlugin({
         //bypass process check, https://github.com/Microsoft/monaco-editor-webpack-plugin/issues/28
