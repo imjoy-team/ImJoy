2f76777c0e59eb8580b5bdce46d67b0c1ccafebe
oeway <oeway007@gmail.com>
2018-12-27 22:02:48 +0100

    add karma for unit test

--- web/vue.config.js
+++ web/vue.config.js
@@ -25,5 +25,49 @@ module.exports = {
         'process.platform': 0
       })
     ]
+  },
+  pluginOptions: {
+    karma: {
+      karmaConfig: {
+        frameworks: ['mocha'],
+        files: [
+          'tests/unit/*.spec.js'
+        ],
+        preprocessors: {
+          '**/*.spec.js': ['webpack', 'sourcemap']
+        },
+
+        browsers: ['ChromeNoSandbox', 'Firefox'],
+
+        customLaunchers: {
+          ChromeNoSandbox: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+          }
+        },
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+        // web server port
+        port: 9876,
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+      }
+    }
   }
 }
