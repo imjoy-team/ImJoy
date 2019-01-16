524ede1c66e3c73442905c19a90ad5c1b27c9cbd
oeway <oeway007@gmail.com>
2019-01-16 19:35:53 +0100

    support export log

--- web/src/components/windows/LogWindow.vue
+++ web/src/components/windows/LogWindow.vue
@@ -5,8 +5,11 @@
         <p :class="t.type==='error'?'error': 'info'">{{t.value || t}}</p>
       </li>
     </ul>
-    <md-button class="md-mini" v-if="w.data && w.data.log_history && w.data.log_history.length>0" @click="clear">
+    <md-button class="md-mini" v-if="w.data && w.data.log_history && w.data.log_history.length>0" @click="clearLog()">
         <md-icon>clear</md-icon>Clear
+    </md-button>
+    <md-button class="md-mini" v-if="w.data && w.data.log_history && w.data.log_history.length>0" @click="exportLog()">
+        <md-icon>cloud_download</md-icon>Export
     </md-button>
     <p v-else>
       No log available.
@@ -15,7 +18,7 @@
 </template>
 
 <script>
-
+import { saveAs } from 'file-saver';
 export default {
   name: 'log-window',
   type: 'imjoy/log',
@@ -28,10 +31,19 @@ export default {
     }
   },
   methods: {
-    clear(){
+    clearLog(){
       this.w.data.log_history.splice(0, this.w.data.log_history.length)
       this.w.data.log_history._error = ''
       this.w.data.log_history._info = ''
+    },
+    exportLog(){
+      const filename = this.w.name+'_log.txt'
+      let content = ''
+      for(let c of this.w.data.log_history){
+        content = content + c.value + '\n'
+      }
+      const file = new Blob([content], {type: "text/plain;charset=utf-8"})
+      saveAs(file, filename);
     }
   }
 }
