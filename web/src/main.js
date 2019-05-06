2fcd129befb6ba0873c8720c7f660c1f01b5f0dc
Wei Ouyang <oeway007@gmail.com>
2019-05-06 15:27:14 +0200

    Use vuematerial from the dev branch of vuematerial/vue-material (#89)
    * switch vuematerial to github version
    
    * clean up packages; fix vulnerabilities

--- web/src/main.js
+++ web/src/main.js
@@ -8,8 +8,6 @@ import 'vue-material/dist/theme/default.css'
 
 import App from './App'
 import VueGridLayout from 'vue-grid-layout'
-import Grid from "vue-js-grid"
-import vueSlider from 'vue-slider-component'
 import store from './store.js'
 
 import Navbar from '@/components/Navbar'
@@ -29,10 +27,8 @@ import './registerServiceWorker'
 Vue.config.productionTip = false
 
 Vue.use(VueMaterial)
-Vue.use(Grid)
 
 // register
-Vue.component('vue-slider', vueSlider)
 Vue.component('navbar', Navbar)
 Vue.component('imjoy', Imjoy)
 Vue.component('main-footer', Footer)
