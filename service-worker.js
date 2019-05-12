e73d47935b0dbfdc43fb5cf316032c689dfbee89
Travis CI <travis@travis-ci.org>
2019-05-12 12:29:15 +0000

    Deploy to GitHub Pages gh-pages branch: d59c6130dc758847bca54996753e53bcf73fe445

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.ae6f2676cd317854df2f2c478234ef74.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.dd3194cedda2067884208a14aaef24f7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 
