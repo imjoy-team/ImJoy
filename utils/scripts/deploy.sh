609d16664949e636b81c6d8cd42c871daa964264
oeway <oeway007@gmail.com>
2019-07-24 18:37:57 +0200

    remove old script

--- utils/scripts/deploy.sh
+++ utils/scripts/deploy.sh
@@ -24,6 +24,7 @@ ssh-add ./imjoy_github_deploy
 # Clone the existing gh-pages for this repo into gh-pages/
 git clone $REPO gh-pages
 cd gh-pages
+
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy).
 git checkout $TARGET_BRANCH || { git checkout --orphan $TARGET_BRANCH; git rm -rf .; }
 git config user.name "Travis CI"
@@ -45,38 +46,3 @@ git diff-index --quiet HEAD || git commit -m "Deploy to GitHub Pages $TARGET_BRA
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
-
-# ======== deploy imjoy-lib with jailed library ========
-LIB_REPO=git@github.com:oeway/lib.imjoy.io.git
-cd ../
-git clone $LIB_REPO imjoy-lib
-cd imjoy-lib
-
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy).
-git checkout master || { git checkout --orphan master; git rm -rf .; }
-
-# Clean up
-rm -rf ./*
-
-# Copy dirs and files and that we want to update.
-cp -Rf ../web/dist/*  ./
-rm -rf ./static/*
-rm -rf ./docs
-rm -rf CNAME
-rm -rf index.html
-cp -Rf ../web/dist/static/icons ./static/icons
-cp -Rf ../web/dist/static/iconfont ./static/iconfont
-cp -Rf ../web/dist/static/jailed ./static/jailed
-
-# Create .nojekyll to bypass Github jekyll
-touch .nojekyll
-echo "lib.imjoy.io" > CNAME
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add -A .
-git diff-index --quiet HEAD || git commit -m "Deploy to GitHub Pages master branch: ${SHA}"
-
-LIB_SSH_REPO=${LIB_REPO/https:\/\/github.com\//git@github.com:}
-# Now that we're all set up, we can push.
-git push $LIB_SSH_REPO master
