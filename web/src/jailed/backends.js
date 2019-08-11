4bab4f6fc588ec7252c6ce72e5aa9669890d9fe5
Wei Ouyang <oeway007@gmail.com>
2019-08-11 22:47:36 +0200

    fix pio

--- web/src/jailed/backends.js
+++ web/src/jailed/backends.js
@@ -378,6 +378,56 @@ class SocketioConnection {
   }
 }
 
+class PostMessageIO {
+  constructor() {
+    this._callbacks = {};
+    window.addEventListener(
+      "message",
+      e => {
+        if (e.source === window.parent) {
+          if (e.data.channel && this._callbacks[e.data.channel]) {
+            for (let cb of this._callbacks[e.data.channel]) cb(e.data);
+          }
+        }
+      },
+      false
+    );
+  }
+  connect() {
+    window.parent.postMessage({ type: "pio_connect" }, "*");
+  }
+
+  disconnect() {
+    window.parent.postMessage({ type: "pio_disconnect" }, "*");
+  }
+
+  emit(channel, data, callback) {
+    data.channel = channel;
+    window.parent.postMessage(data, "*");
+  }
+
+  on(channel, callback) {
+    if (this._callbacks[channel]) {
+      this._callbacks[channel].push(callback);
+    } else {
+      this._callbacks[channel] = [callback];
+    }
+  }
+
+  off(channel, callback) {
+    if (this._callbacks[channel]) {
+      for (var i = 0; i < this._callbacks[channel].length; i++) {
+        if (this._callbacks[channel][i] === callback) {
+          this._callbacks[channel].splice(i, 1);
+        }
+      }
+    }
+  }
+}
+
+const pio = new PostMessageIO();
+pio.connect();
+
 class PostMessageConnection {
   constructor(id, type, config) {
     assert(
@@ -390,27 +440,39 @@ class PostMessageConnection {
     this.id = id;
     var me = this;
 
-    window.addEventListener(
-      "message",
-      function(e) {
-        if (e.source === window.parent) {
-          if (e.data.type == "init_plugin") {
-            console.log("initializing plugin: ", e.data.config);
-          } else if (e.data.type == "initialized") {
-            me.dedicatedThread = e.data.dedicatedThread;
-            me._init();
-          } else {
-            me._messageHandler(e.data);
-          }
-        }
-      },
-      false
-    );
+    const config_ = {
+      api_version: config.api_version,
+      flags: config.flags,
+      tag: config.tag,
+      workspace: config.workspace,
+      env: config.env,
+      requirements: config.requirements,
+      cmd: config.cmd,
+      name: config.name,
+      type: config.type,
+      inputs: config.inputs,
+      outputs: config.outputs,
+    };
 
-    window.parent.postMessage(
-      { type: "message", data: { type: "init_connection" } },
-      "*"
-    );
+    // create a plugin here
+    pio.emit("init_plugin", { id: this.id, type: type, config: config_ });
+
+    this._disconnected = false;
+    config.work_dir = "";
+    config.resumed = false;
+
+    pio.on("message_from_plugin_" + this.id, data => {
+      if (data.type == "initialized") {
+        this.dedicatedThread = data.dedicatedThread;
+        this._init();
+      } else if (data.type == "logging") {
+        this._loggingHandler(data.details);
+      } else if (data.type == "disconnected") {
+        this._disconnectHandler(data.details);
+      } else {
+        this._messageHandler(data);
+      }
+    });
   }
 
   whenInit(handler) {
@@ -421,12 +483,15 @@ class PostMessageConnection {
     this._fail.whenEmitted(handler);
   }
 
-  send(data, transferables) {
-    window.parent.postMessage(
-      { type: "message", data: data },
-      "*",
-      transferables
-    );
+  send(data) {
+    if (pio) {
+      pio.emit("message_to_plugin_" + this.id, {
+        type: "message",
+        data: data,
+      });
+    } else {
+      throw "socketio disconnected.";
+    }
   }
 
   onMessage(handler) {
