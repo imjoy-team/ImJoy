caf86fb13596a03c63e507590418a228fe639585
oeway <oeway007@gmail.com>
2020-01-12 07:23:27 +0100

    add plugin file preview code

--- web/public/static/js/imjoy-preview.js
+++
@@ -1,8 +0,0 @@
-if(window && window.location && window.location.protocol === 'file:'){
-    const preview = window.open("https://imjoy.io/#/preview");
-    window.addEventListener("message", function(ev) {
-        if (ev.data.type === "imjoy-app-ready") {
-            preview.postMessage({ type: "load-imjoy-plugin", code: document.body.innerHTML }, "*");
-        }
-    });
-}
