538583411f7544792be56a2aef79dbc10e24c8e2
Travis CI <travis@travis-ci.org>
2020-01-23 09:31:20 +0000

    Deploy to GitHub Pages gh-pages branch: fff1d1b98499e73e65e2eef5acd3ce20d556318b

---
+++ precache-manifest.0c4274b0bfdca830d224c49b216c30fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a837c972ff14c16d3780",
+    "url": "imjoy-lib.min.js"
+  },
+  {
+    "revision": "bd1ef755490ff03edb0cc55793b4e734",
+    "url": "static/jailed/jailed.js"
+  },
+  {
+    "revision": "57b010e2ad23bba6a8b626b01a83bf4d",
+    "url": "static/jailed/_frame.html"
+  },
+  {
+    "revision": "3e495b5f1dfa84ec6c49665e974efdb3",
+    "url": "static/jailed/_frame.js"
+  },
+  {
+    "revision": "2319e6b2c5053a4d57fa0494b76ce8c9",
+    "url": "static/jailed/_JailedSite.js"
+  },
+  {
+    "revision": "3c947349d7fe3b84b65a0501ec4958f3",
+    "url": "static/jailed/_pluginCore.js"
+  },
+  {
+    "revision": "3908d573836fcb88b63a94c565ad43f4",
+    "url": "static/jailed/_pluginNode.js"
+  },
+  {
+    "revision": "338f5de11f5e94533510a4d7add6407f",
+    "url": "static/jailed/_pluginWebIframe.js"
+  },
+  {
+    "revision": "02e7132e1ac3632aba8d480fcc03a47d",
+    "url": "static/jailed/_pluginWebPython.js"
+  },
+  {
+    "revision": "fe7b7b24efd69da34f24d8f782067799",
+    "url": "static/jailed/_pluginWebWorker.js"
+  },
+  {
+    "revision": "333950bdcd038dec3a096f81c6a74df5",
+    "url": "static/jailed/backends.js"
+  },
+  {
+    "revision": "f9bf1db80923c73a2a8aac899bae0c16",
+    "url": "joy.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.f0e967e1aa956807d3b87858b9d40c30.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.c366ba8989cc7bf120d0ca021fc8c783.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 
