70b31617de1ab7b16cb58d21656cd002d182334a
Wei Ouyang <oeway007@gmail.com>
2020-02-19 23:22:19 +0100

    improve upload message

--- web/src/components/FileDialog.vue
+++ web/src/components/FileDialog.vue
@@ -218,6 +218,7 @@ export default {
             );
           }
         }
+        this.status_text = `Uploaded ${files.length} files.`;
       } catch (e) {
         this.status_text =
           "Failed to upload, error:" + (e ? e.toString() : "undefined");
