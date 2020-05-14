3496733058b8710d69842ea890d71a528099a3ff
Wei Ouyang <oeway007@gmail.com>
2020-05-14 21:00:19 +0200

    Upgade imjoy-core and fix imjoy-lite api version (#359)
    * remove imjoy-rpc
    
    * bump api_version
    
    * pin core version to 0.13.1
    
    * upgrade core version
    
    * rename asset_url; small fixes
    
    * increase timeout
    
    * change node version
    
    * Set node version
    
    * Remove tests for the core
    
    * Fix linter

--- web/tests/ImJoy.spec.js
+++ web/tests/ImJoy.spec.js
@@ -3,7 +3,7 @@ import { shallowMount, createLocalVue } from "@vue/test-utils";
 import VueRouter from "vue-router";
 import VueMaterial from "vue-material";
 import { router_config } from "../src/router";
-import { Joy, version } from "imjoy-core";
+import { version } from "imjoy-core";
 import Imjoy from "@/components/Imjoy.vue";
 import _ from "lodash";
 
@@ -13,9 +13,6 @@ import WINDOW_PLUGIN_TEMPLATE from "../src/plugins/windowTemplate.imjoy.html";
 import WEB_PYTHON_PLUGIN_TEMPLATE from "../src/plugins/webPythonTemplate.imjoy.html";
 import WEB_PYTHON_WINDOW_PLUGIN_TEMPLATE from "../src/plugins/webPythonWindowTemplate.imjoy.html";
 
-import TEST_WEB_WORKER_PLUGIN_1 from "./testWebWorkerPlugin1.imjoy.html";
-import TEST_WEB_WORKER_PLUGIN_2 from "./testWebWorkerPlugin2.imjoy.html";
-import TEST_WINDOW_PLUGIN_1 from "./testWindowPlugin1.imjoy.html";
 const localVue = createLocalVue();
 
 console.log("ImJoy Core version: " + version);
@@ -34,7 +31,7 @@ describe("ImJoy.vue", async () => {
   const pm = vm.pm; //plugin_manager
 
   before(function(done) {
-    this.timeout(10000);
+    this.timeout(30000);
     vm.event_bus.on("imjoy_ready", () => {
       expect(vm.plugin_loaded).to.be.true;
       done();
@@ -145,111 +142,5 @@ describe("ImJoy.vue", async () => {
     expect(typeof plugin.api.run).to.equal("function");
     await plugin.api.run({});
     plugin.terminate();
-  }).timeout(20000);
-
-  describe("Test ImJoy API", async () => {
-    let plugin1;
-    let plugin2;
-    let pluginw;
-    before(function(done) {
-      this.timeout(20000);
-      pm.reloadPlugin({ code: _.clone(TEST_WEB_WORKER_PLUGIN_1) }).then(p1 => {
-        plugin1 = p1;
-        expect(plugin1.name).to.equal("Test Web Worker Plugin 1");
-        expect(plugin1.type).to.equal("web-worker");
-        expect(Joy.getTemplateByType(plugin1.name).init).to.include(
-          "op-ui-option1"
-        );
-        expect(typeof plugin1.api.run).to.equal("function");
-        pm.reloadPlugin({ code: _.clone(TEST_WEB_WORKER_PLUGIN_2) }).then(
-          p2 => {
-            plugin2 = p2;
-            expect(plugin2.name).to.equal("Test Web Worker Plugin 2");
-            expect(plugin2.type).to.equal("web-worker");
-            expect(typeof plugin2.api.run).to.equal("function");
-
-            pm.reloadPlugin({ code: _.clone(TEST_WINDOW_PLUGIN_1) }).then(
-              pw => {
-                pluginw = pw;
-                expect(pluginw.name).to.equal("Test Window Plugin");
-                expect(pluginw.type).to.equal("window");
-                expect(typeof pluginw.api.run).to.equal("function");
-                pm.createWindow(null, {
-                  name: "new window",
-                  type: "Test Window Plugin",
-                }).then(wplugin => {
-                  expect(typeof wplugin.add2).to.equal("function");
-                  done();
-                });
-              }
-            );
-          }
-        );
-      });
-    });
-
-    it("should register and unregister", async () => {
-      expect(Object.keys(plugin1.ops).length).to.equal(1);
-      expect(await plugin1.api.test_register()).to.be.true;
-      expect(Joy.getTemplateByType(plugin1.name + "/LUT").init).to.include(
-        "apply LUT"
-      );
-      expect(Object.keys(plugin1.ops).length).to.equal(2);
-      expect(await plugin1.api.test_unregister()).to.be.true;
-      expect(Object.keys(plugin1.ops).length).to.equal(1);
-      expect(function() {
-        Joy.getTemplateByType(plugin1.name + "/LUT");
-      }).to.throw();
-    });
-
-    it("should create imjoy window", async () => {
-      const count = wm.windows.length;
-      expect(await plugin1.api.test_create_imjoy_window()).to.be.true;
-      expect(wm.windows.length).to.equal(count + 1);
-      expect(wm.windows[wm.windows.length - 1].name).to.equal("new image");
-      await wm.windows[wm.windows.length - 1].close();
-      expect(wm.windows.length).to.equal(count);
-    });
-
-    it("should create window", async () => {
-      const count = wm.windows.length;
-      expect(await plugin1.api.test_create_window()).to.be.true;
-      expect(wm.windows.length).to.equal(count + 1);
-      expect(wm.windows[wm.windows.length - 1].name).to.equal("new window");
-      await wm.windows[wm.windows.length - 1].close();
-      expect(wm.windows.length).to.equal(count);
-    });
-
-    it("should close imjoy window", async () => {
-      expect(await plugin1.api.test_close_imjoy_window()).to.be.true;
-    });
-
-    it("should close window", async () => {
-      expect(await plugin1.api.test_close_window()).to.be.true;
-    });
-
-    it("should run plugin", async () => {
-      expect(await plugin1.api.test_run()).to.be.true;
-    });
-
-    it("should call plugin", async () => {
-      expect(await plugin1.api.test_call()).to.be.true;
-    });
-
-    it("should get plugin", async () => {
-      expect(await plugin1.api.test_get_plugin()).to.be.true;
-    });
-
-    it("should set/get config", async () => {
-      expect(await plugin1.api.test_config()).to.be.true;
-    });
-
-    it("should get attachment", async () => {
-      expect(await plugin1.api.test_get_attachment()).to.be.true;
-    });
-
-    it("should read and write with BrowserFS plugin", async () => {
-      expect(await plugin1.api.test_fs()).to.be.true;
-    });
-  });
+  }).timeout(100000);
 });
